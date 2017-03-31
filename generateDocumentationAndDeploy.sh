8cc741baac1fbe77fae32fcd8dc7d0aa5cf9677e
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2017-03-31 11:14:32 -0500

    Change output for documentation

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -40,8 +40,8 @@ echo 'Setting up the script...'
 set -e
 
 # Create a clean working directory for this script.
-mkdir code_docs
-cd code_docs
+#mkdir code_docs
+#cd code_docs
 
 # Get the current gh-pages branch
 git clone -b gh-pages https://git@$GH_REPO_REF

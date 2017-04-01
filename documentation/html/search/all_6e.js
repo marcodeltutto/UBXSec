53dfec4e6ef2a6a16413c11ef7745d44ab4271fc
Travis CI <travis@travis-ci.org>
2017-04-01 00:17:42 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: a0fd872ea760d09b4ebd79e134bd17b659870d9e

--- documentation/html/search/all_6e.js
+++ documentation/html/search/all_6e.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['neutrinoflashmatch',['NeutrinoFlashMatch',['../classNeutrinoFlashMatch.html',1,'']]],
-  ['neutrinoflashmatchana',['NeutrinoFlashMatchAna',['../classNeutrinoFlashMatchAna.html',1,'']]],
   ['neutrinomcflash',['NeutrinoMCFlash',['../classNeutrinoMCFlash.html',1,'']]]
 ];

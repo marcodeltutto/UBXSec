2d115d8d1c0e97790d36e926759f974492efe539
Travis CI <travis@travis-ci.org>
2017-07-13 10:44:57 +0000

    Deploy code docs to GitHub Pages Travis build: 31
    Commit: 5890f1879a1b014eef6453596ac16bb94ddf046c

--- documentation/html/search/all_63.js
+++ documentation/html/search/all_63.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['clear',['Clear',['../classubxsec_1_1ACPTAlgo.html#a749010492996f04d6c67a614647f7fe8',1,'ubxsec::ACPTAlgo::Clear()'],['../classubxsec_1_1VertexCheck.html#abb5969f8c850d1191248bddd91ec49d1',1,'ubxsec::VertexCheck::Clear()']]],
-  ['collecttracksandpfp',['CollectTracksAndPFP',['../classubana_1_1TPCObjectMaker.html#a39991fa551ce05bcb4fb10db4f80ab49',1,'ubana::TPCObjectMaker::CollectTracksAndPFP()'],['../classUBXSecHelper.html#ab441342f0c71741e353f323a26f27728',1,'UBXSecHelper::CollectTracksAndPFP()']]],
+  ['collecttracksandpfp',['CollectTracksAndPFP',['../group__UBXSec.html#ga39991fa551ce05bcb4fb10db4f80ab49',1,'ubana::TPCObjectMaker::CollectTracksAndPFP()'],['../classUBXSecHelper.html#ab441342f0c71741e353f323a26f27728',1,'UBXSecHelper::CollectTracksAndPFP()']]],
   ['configure',['Configure',['../classubxsec_1_1ACPTAlgo.html#a769831f453da3866e53fd99d7b092e7a',1,'ubxsec::ACPTAlgo::Configure()'],['../classFindDeadRegions.html#a94b207b673c58df861537ead4ec5385a',1,'FindDeadRegions::Configure()'],['../classubxsec_1_1McPfpMatch.html#a15cea806e5a1b12bf2e187d5d26c528d',1,'ubxsec::McPfpMatch::Configure()'],['../classubxsec_1_1VertexCheck.html#aa2b80d7c8c6eb06d53d44f5ea2810b22',1,'ubxsec::VertexCheck::Configure()']]],
   ['cosmicflashmatch',['CosmicFlashMatch',['../classCosmicFlashMatch.html',1,'']]]
 ];

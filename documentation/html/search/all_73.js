5afb75efa49be2189899211a93dc93b4ec2104f4
Travis CI <travis@travis-ci.org>
2017-08-19 21:53:08 +0000

    Deploy code docs to GitHub Pages Travis build: 38
    Commit: aae2a2cc4af548adbdca5b45168cec9afdc5688c

--- documentation/html/search/all_73.js
+++ documentation/html/search/all_73.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['settpcobj',['SetTPCObj',['../classubxsec_1_1ACPTAlgo.html#ae92b36aadcc1dadfb7e1ca5c72c29fb3',1,'ubxsec::ACPTAlgo::SetTPCObj()'],['../classubxsec_1_1VertexCheck.html#afe6992fb059684ac84b4cbf873ca190c',1,'ubxsec::VertexCheck::SetTPCObj()']]],
-  ['setvtx',['SetVtx',['../classubxsec_1_1ACPTAlgo.html#ad1a4982ef30c3b8395eb346ae155c50e',1,'ubxsec::ACPTAlgo::SetVtx()'],['../classubxsec_1_1VertexCheck.html#aa7ba14f2c0dc87099769e11a78a7e156',1,'ubxsec::VertexCheck::SetVtx()']]],
-  ['simplehit',['SimpleHit',['../structubana_1_1SimpleHit.html',1,'ubana']]]
+  ['setvtx',['SetVtx',['../classubxsec_1_1ACPTAlgo.html#ad1a4982ef30c3b8395eb346ae155c50e',1,'ubxsec::ACPTAlgo::SetVtx()'],['../classubxsec_1_1VertexCheck.html#aa7ba14f2c0dc87099769e11a78a7e156',1,'ubxsec::VertexCheck::SetVtx()']]]
 ];
--- documentation/html/search/classes_73.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['simplehit',['SimpleHit',['../structubana_1_1SimpleHit.html',1,'ubana']]]
-];

b11162a5027bba57c9a79d91fa69a446c74f5b3e
Travis CI <travis@travis-ci.org>
2017-08-19 20:48:45 +0000

    Deploy code docs to GitHub Pages Travis build: 37
    Commit: dc0a3c86540fd6f7d9415d098c2d4cba7ba4eb27

--- documentation/html/search/classes_63.js
+++ documentation/html/search/classes_63.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['cosmicflashmatch',['CosmicFlashMatch',['../classCosmicFlashMatch.html',1,'']]]
+  ['cosmicflashmatch',['CosmicFlashMatch',['../classCosmicFlashMatch.html',1,'']]],
+  ['cosmictagbyhitintegral',['CosmicTagByHitIntegral',['../classubana_1_1CosmicTagByHitIntegral.html',1,'ubana']]],
+  ['cosmictagbyhitintegral',['CosmicTagByHitIntegral',['../classCosmicTagByHitIntegral.html',1,'']]],
+  ['cosmictagtoolinterface',['CosmicTagToolInterface',['../classCosmicTagToolInterface.html',1,'']]],
+  ['cosmictrackhittagger',['CosmicTrackHitTagger',['../classCosmicTrackHitTagger.html',1,'']]]
 ];
---
+++ documentation/html/search/classes_73.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['simplehit',['SimpleHit',['../structubana_1_1SimpleHit.html',1,'ubana']]]
+];

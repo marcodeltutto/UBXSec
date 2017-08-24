06aee13169ea81afa8d2da9fcad3d090607d0c8c
Travis CI <travis@travis-ci.org>
2017-08-24 16:21:23 +0000

    Deploy code docs to GitHub Pages Travis build: 45
    Commit: b68deaa6a86f8b50c2d4f5677da01ff81a8ff285

--- documentation/html/search/all_6d.js
+++ documentation/html/search/all_6d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['mcghost',['MCGhost',['../classubana_1_1MCGhost.html',1,'ubana']]],
   ['mcpfpmatch',['McPfpMatch',['../classMcPfpMatch.html',1,'McPfpMatch'],['../classubxsec_1_1McPfpMatch.html#aea3e9923fdf8855d5798015b36f2d516',1,'ubxsec::McPfpMatch::McPfpMatch()']]],
   ['mcpfpmatch',['McPfpMatch',['../classubxsec_1_1McPfpMatch.html',1,'ubxsec']]]
 ];
--- documentation/html/search/classes_6d.js
+++ documentation/html/search/classes_6d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['mcghost',['MCGhost',['../classubana_1_1MCGhost.html',1,'ubana']]],
   ['mcpfpmatch',['McPfpMatch',['../classMcPfpMatch.html',1,'']]],
   ['mcpfpmatch',['McPfpMatch',['../classubxsec_1_1McPfpMatch.html',1,'ubxsec']]]
 ];

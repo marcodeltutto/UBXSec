465e9f0ec7b854d8635787b48918f332b8f3b632
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2017-10-31 17:12:56 -0500

    fixing bug - not returns const obj

--- DataTypes/SelectionResult.cxx
+++ DataTypes/SelectionResult.cxx
@@ -8,6 +8,11 @@ namespace ubana {
   SelectionResult::~SelectionResult(){
   }
 
+  const std::string & SelectionResult::GetSelectionType()   const {return _selection_type;   }
+
+  const bool        & SelectionResult::GetSelectionStatus() const {return _selection_status; }
+
+  const std::string & SelectionResult::GetFailureReason()   const {return _failure_reason;   }
 }
 
 
